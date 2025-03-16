a27ef2afecf5c12472d7d12b6a61e3d2497f5ec2
yeganeh-mahditabar <yeganehhhhh.2001@gmail.com>
2025-03-17 00:29:21 +0330

    handling comment error

--- store2-react-app/src/Components/Comments/Comments.jsx
+++ store2-react-app/src/Components/Comments/Comments.jsx
@@ -1,39 +1,51 @@
+import { useEffect, useState } from "react";
 import Errorbox from "../Errorbox/Errorbox";
+import { API_URL } from "../../configs";
 
 export default function Comments() {
+  const [allComments, setAllComments] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/comments`)
+      .then((res) => res.json())
+      .then((comments) => setAllComments(comments));
+  }, []);
 
   return (
     <div className="cms-main">
-      <Errorbox msg="هیچ کامنتی یافت نشد" />
+      {allComments.length ? (
+        <table className="cms-table">
+          <thead>
+            <tr>
+              <th>اسم کاربر</th>
+              <th>محصول</th>
+              <th>کامنت</th>
+              <th>تاریخ</th>
+              <th>ساعت</th>
+            </tr>
+          </thead>
 
-      <table className="cms-table">
-        <thead>
-          <tr>
-            <th>اسم کاربر</th>
-            <th>محصول</th>
-            <th>کامنت</th>
-            <th>تاریخ</th>
-            <th>ساعت</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>مریم</td>
-            <td>آیفون 13</td>
-            <td><button>دیدن متن</button></td>
-            <td>1403-11-11</td>
-            <td>14:12</td>
-            <td>
-              <button>حذف</button>
-              <button>ویرایش</button>
-              <button>پاسخ</button>
-              <button>تایید</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
+          <tbody>
+            <tr>
+              <td>مریم</td>
+              <td>آیفون 13</td>
+              <td>
+                <button>دیدن متن</button>
+              </td>
+              <td>1403-11-11</td>
+              <td>14:12</td>
+              <td>
+                <button>حذف</button>
+                <button>ویرایش</button>
+                <button>پاسخ</button>
+                <button>تایید</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <Errorbox msg="هیچ کامنتی یافت نشد" />
+      )}
     </div>
   );
 }
